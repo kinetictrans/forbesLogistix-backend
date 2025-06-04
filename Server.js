5c097211920fba39a9fdaa9769ac7c854301eda2
moominfirdous@gmail.com <moominnaik@Abduls-MacBook-Air.local>
2025-06-04 20:07:44 +0530

    backend repair

--- Server.js
+++ Server.js
@@ -2,32 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const pdfRoutes = require('./routes/pdfRoutes');
-const auth = {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  }
-
 
 dotenv.config();
 const app = express();
-app.use(cors());
+
 app.use(cors({
-    origin: 'https://forbes-logistics-backend-9i9g-8i60yom1j.vercel.app',
-    methods: ['GET', 'POST'],
-  }));
+  origin: 'https://forbes-logistics-frontend.vercel.app', // your frontend domain on vercel
+  methods: ['GET', 'POST'],
+}));
 
-//app.options('/api/send-pdf', cors());
-// ✅ CORS configuration — allow frontend origin
-
-
-
-// Body parser
 app.use(express.json({ limit: '10mb' }));
-//app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use('/api', pdfRoutes);
 
-// Start server
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// DO NOT call app.listen() on Vercel
+module.exports = app;
